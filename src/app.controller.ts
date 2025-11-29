6773b51e43e69f5a78388443d468602020e724bb
is6ream <dnlils10@icloud.com>
2025-11-29 15:35:05 +0500

    feat (app.controller.ts) Registered a controller for the 'users' path

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-
+@Controller('users')
+export class UsersController {
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getAll() {
+    return 'This action return all users';
+  }
+  @Get('default-category')
+  getMyBooks() {
+    return 'This action returns users by default category';
   }
 }
