c5fd39f30f85419ca2b17da83008231d849d7e13
is6ream <dnlils10@icloud.com>
2025-12-01 18:52:06 +0500

    feat (usersController) set types for queryInput

--- src/modules/users/api/user-controller.ts
+++ src/modules/users/api/user-controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Query } from '@nestjs/common';
 import { UsersQueryRepository } from '../infrastructure/usersQueryRepository';
 import { UsersService } from '../application/user-service';
 import { UserViewModel } from '../types/output/userViewModel';
-import { PaginationQueryDto } from '../types/input/userQueryDto';
+import type { UserQueryInput } from 'src/common/pagination/user/userQueryInput';
 @Controller('users')
 export class UserController {
   constructor(
@@ -11,7 +11,7 @@ export class UserController {
   ) {}
 
   @Get()
-  async getAll(@Query() query: PaginationQueryDto): Promise<UserViewModel[]> {
+  async getAll(@Query() query: UserQueryInput): Promise<UserViewModel[]> {
     return await this.usersQueryRepository.getAll(query);
   }
 }
