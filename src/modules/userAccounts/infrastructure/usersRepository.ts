2ddaedf569884c2537ddbd514af129b9ed7a0856
is6ream <dnlils10@icloud.com>
2025-12-05 07:05:39 +0500

    feat (usersRepository) prescribed all necessary methods according to swagger

--- src/modules/userAccounts/infrastructure/usersRepository.ts
+++ src/modules/userAccounts/infrastructure/usersRepository.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { User } from '../domain/userEntity';
+import { User, UserModelType } from '../domain/userEntity';
+import { UserDocument } from '../entities/userMongoose';
 //остановился  на настройке импортов в этом слое, продолжить рефакторинг
 
 @Injectable()
 export class UsersRepository {
-  constructor(@InjectModel(User.name))
+  constructor(@InjectModel(User.name) private UserModel: UserModelType) {}
+  //Данная модель нам понадобится для использования в репо
+
+  async findById(id: string): Promise<UserDocument | null> {
+    return this.UserModel.findOne({
+      _id: id,
+      deleteAt: null,
+    });
+  }
+
+  async save(user: UserDocument) {
+    await user.save();
+  }
+
+  async findOrNotFoundFail(id: string): Promise<UserDocument> {
+    const user = await this.findById(id);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    return user;
+  }
 }
