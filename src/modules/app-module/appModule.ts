c719c3a589d4ee8b11c128fedaaa9f47209c8d68
is6ream <dnlils10@icloud.com>
2025-12-13 15:31:57 +0500

    feat(platform): create bloggers platform module and register in main AppModule

--- src/modules/app-module/appModule.ts
+++ src/modules/app-module/appModule.ts
@@ -4,11 +4,13 @@ import { UserAccountsModule } from '../user-accounts/userAccounts.module';
 import { AppController } from './appController';
 import { AppService } from './appService';
 import { MongooseModule } from '@nestjs/mongoose';
+import { BloggersPlatformModule } from '../bloggers-platform/bloggers-platform.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/blogger-platform'),
     UserAccountsModule,
+    BloggersPlatformModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/modules/bloggers-platform/bloggers-platform.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BlogSchema } from './blogs/domain/blogEntity';
+import { Blog } from './blogs/domain/blogEntity';
+import { BlogsController } from './blogs/api/blogs-controller';
+import { BlogsQueryRepository } from './blogs/infrastructure/blgosQueryRepository';
+import { BlogsRepository } from './blogs/infrastructure/blogsRepository';
+import { BlogsService } from './blogs/application/blogs-service';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Blog.name, schema: BlogSchema }]),
+  ],
+  controllers: [BlogsController],
+  providers: [BlogsQueryRepository, BlogsRepository, BlogsService],
+  exports: [],
+})
+export class BloggersPlatformModule {}
