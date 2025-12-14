d0b118c368a77a73ab2217a67658543f537beef4
is6ream <dnlils10@icloud.com>
2025-12-14 17:30:50 +0500

    feat(postsService): implement createPost method

--- src/modules/bloggers-platform/posts/application/posts-service.ts
+++ src/modules/bloggers-platform/posts/application/posts-service.ts
@@ -3,20 +3,22 @@ import { InjectModel } from '@nestjs/mongoose';
 import { PostDocument, PostModelType } from '../domain/postEntity';
 import { Post } from '../domain/postEntity';
 import { CreatePostDto } from '../domain/dto/createPostDto';
-import { BlogDocument, BlogModelType } from '../../blogs/domain/blogEntity';
+import { BlogDocument } from '../../blogs/domain/blogEntity';
 import { BlogsRepository } from '../../blogs/infrastructure/blogsRepository';
-
+import { PostRepository } from '../infrastructure/postRepository';
 @Injectable()
 export class PostsService {
   constructor(
     @InjectModel(Post.name)
     private PostModel: PostModelType,
-    private blogsRepository: BlogsRepository
-    private PostRepository: PostRepository,
+    private blogsRepository: BlogsRepository,
+    private postRepository: PostRepository,
   ) {}
 
   async createPost(dto: CreatePostDto): Promise<string> {
-    const blog: BlogDocument = await this.blogsRepository.findOrNotFoundFail(dto.blogId)
+    const blog: BlogDocument = await this.blogsRepository.findOrNotFoundFail(
+      dto.blogId,
+    );
     const post: PostDocument = this.PostModel.createInstance({
       title: dto.title,
       shortDescription: dto.shortDescription,
@@ -25,6 +27,8 @@ export class PostsService {
       blogName: blog.name,
     });
 
-    
+    await this.postRepository.save(post);
+
+    return post._id.toString();
   }
 }
