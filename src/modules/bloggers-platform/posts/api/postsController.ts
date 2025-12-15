506b301934b583f729d9a66bf364f561aa83471b
is6ream <dnlils10@icloud.com>
2025-12-15 07:24:45 +0500

    feat(postsController): implement update method for posts by ID

--- src/modules/bloggers-platform/posts/api/postsController.ts
+++ src/modules/bloggers-platform/posts/api/postsController.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Post, Query, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Put, Query, Body, Param, HttpCode, HttpStatus } from '@nestjs/common';
 import { GetPostsQueryParams } from './query/get-posts-query-params';
 import { PostPaginatedViewDto } from './paginated/paginated.post.view-dto';
 import { CreatePostInputDto } from '../dto/input/createPostInputDto';
 import { PostViewModel } from './model/postViewModel';
 import { PostQueryRepository } from '../infrastructure/postQueryRepository';
 import { PostsService } from '../application/posts-service';
+import { UpdatePostInputDto } from '../dto/input/updatePostInputDto';
 
 @Controller('posts')
 export class PostsController {
@@ -30,5 +31,14 @@ export class PostsController {
   @Get(':id')
   async getById(@Param('id') id: string): Promise<PostViewModel> {
     return this.postQueryRepository.getByIdOrNotFoundFail(id);
+  }
+
+  @Put(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async updatePost(
+    @Param('id') id: string,
+    @Body() body: UpdatePostInputDto
+  ): Promise<void>{
+    return this.postsService.updatePost(id, body)
   }
 }
