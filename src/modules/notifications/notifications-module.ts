6fd035e58e4b8592d2fc05dd0c334badca30b26e
is6ream <dnlils10@icloud.com>
2025-12-24 07:00:24 +0500

    feat(notifications-nodule): Registered a mailer module for sending notifications

--- src/modules/notifications/notifications-module.ts
+++ src/modules/notifications/notifications-module.ts
@@ -2,16 +2,19 @@ import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
 
 @Module({
-    imports: [
-        MailerModule.forRoot({
-            transport: {
-                host: 'smtp.gmail.com',
-                port: 587,
-                secure: false,
-                auth: {
-                    user: 
-                }
-            }
-        })
-    ]
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+          user: process.env.GMAIL_LOGIN,
+          pass: process.env.GMAIL_PASSWORD,
+        },
+      },
+    }),
+  ],
+  exports: [],
 })
+export class NotificationsModule {}
