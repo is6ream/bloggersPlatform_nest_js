6044648121b4293ad54eb7837d60fb65fcb1cf1c
is6ream <dnlils10@icloud.com>
2026-01-12 19:44:08 +0500

    prescribed a updateBlogDto

--- src/modules/bloggers-platform/blogs/dto/input/updateBlogDto.ts
+++ src/modules/bloggers-platform/blogs/dto/input/updateBlogDto.ts
@@ -1,5 +1,13 @@
+import { IsString, Length } from 'class-validator';
+
 export class UpdateBlogDto {
+  @Length(1, 15)
+  @IsString()
   name: string;
+  @Length(1, 500)
+  @IsString()
   description: string;
+  @Length(1, 100)
+  @IsString()
   websiteUrl: string;
 }
