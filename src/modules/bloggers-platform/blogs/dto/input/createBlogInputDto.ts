234f394ae4069792abe0dfce8788b7005b52feea
is6ream <dnlils10@icloud.com>
2026-01-26 20:02:58 +0500

    connected a validation check to createlogInputDto

--- src/modules/bloggers-platform/blogs/dto/input/createBlogInputDto.ts
+++ src/modules/bloggers-platform/blogs/dto/input/createBlogInputDto.ts
@@ -1,5 +1,13 @@
+import { Length } from 'class-validator';
+import { IsString } from 'class-validator';
 export class CreateBlogInputDto {
+  @Length(1, 15)
+  @IsString()
   name: string;
+  @Length(1, 500)
+  @IsString()
   description: string;
+  @Length(1, 100)
+  @IsString()
   websiteUrl: string;
 }
