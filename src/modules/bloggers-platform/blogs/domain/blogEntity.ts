50a3c4f621c45df32a9eddf43aa2ba61734e4569
is6ream <dnlils10@icloud.com>
2026-02-02 19:16:40 +0500

    now createdblog return to the client with 'isMembership' status === false

--- src/modules/bloggers-platform/blogs/domain/blogEntity.ts
+++ src/modules/bloggers-platform/blogs/domain/blogEntity.ts
@@ -33,7 +33,7 @@ export class Blog {
     blog.name = dto.name;
     blog.description = dto.description;
     blog.websiteUrl = dto.websiteUrl;
-    blog.isMembership = true;
+    blog.isMembership = false;
 
     return blog as BlogDocument;
   }
