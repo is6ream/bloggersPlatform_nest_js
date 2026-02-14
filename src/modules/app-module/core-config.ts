b9c8d6ded04e2574a01fdb63deb159d0f712025f
is6ream <dnlils10@icloud.com>
2026-02-14 17:01:29 +0500

    prescribed a custom validation to core-config

--- src/modules/app-module/core-config.ts
+++ src/modules/app-module/core-config.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { IsNotEmpty, IsNumber, IsString, validateSync } from 'class-validator';
 
 @Injectable()
 export class CoreConfig {
-  constructor(private configService: ConfigService) {}
-  port: number = this.configService.getOrThrow('PORT');
+  @IsNumber(
+    {},
+    {
+      message: 'Set env variable PORT',
+    },
+  )
+  port: number = Number(this.configService.getOrThrow('PORT'));
+
+  @IsNotEmpty({
+    message: 'Set Env variable MONGO_URI',
+  })
   mongoURI: string = this.configService.getOrThrow('MONGODB_URI');
+
+  constructor(private configService: ConfigService) {
+    const errors = validateSync(this);
+    if (errors.length > 0) {
+      const sortedMessage = errors
+        .map((err) => Object.values(err.constraints || {}).join(', '))
+        .join('; ');
+    }
+  }
 }
