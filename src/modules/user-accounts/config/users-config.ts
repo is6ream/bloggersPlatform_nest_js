6db75c73ba37efaf8114945636b27c48f7d58b04
is6ream <dnlils10@icloud.com>
2026-02-15 10:39:32 +0500

    prescribed the utility which works with configs

--- src/modules/user-accounts/config/users-config.ts
+++ src/modules/user-accounts/config/users-config.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { IsBoolean, validateSync } from 'class-validator';
 import { ConfigService } from '@nestjs/config';
+import { configValidationUtility } from 'src/core/config/config-validation.utility';
 
 @Injectable()
 export class UserAccountsConfig {
@@ -11,12 +12,6 @@ export class UserAccountsConfig {
     this.configService.getOrThrow('IS_USER_AUTOMATICALLY_CONFIRMED') === 'true';
 
   constructor(private configService: ConfigService) {
-    const errors = validateSync(this);
-    if (errors.length > 0) {
-      const sortedMessages = errors
-        .map((error) => Object.values(error.constraints || {}).join(', '))
-        .join('; ');
-      throw new Error('Validation failed: ' + sortedMessages);
-    }
+    configValidationUtility.validateConfig(this);
   }
 }
