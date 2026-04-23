14fca9a076e286a04ff16f6b62652123366c751b
is6ream <dnlils10@icloud.com>
2026-04-23 07:57:55 +0500

    feat: add User TypeORM entity definition

---
+++ src/modules/user-accounts/infrastructure/users/entities/user.orm-entity.ts
@@ -0,0 +1,38 @@
+import { BaseDBEntity } from 'src/core/database/base-db.entity';
+import { Column, Entity } from 'typeorm';
+
+@Entity('users')
+export class User extends BaseDBEntity {
+  @Column({ type: 'varchar', length: 255 })
+  login!: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  email!: string;
+
+  @Column({ type: 'text' })
+  passwordHash!: string;
+
+  @Column({ type: 'text' })
+  confirmationCode!: string;
+
+  @Column({ type: 'timestamptz' })
+  confirmationExpiration!: Date;
+
+  @Column({ type: 'boolean', default: false })
+  isEmailConfirmed!: boolean;
+
+  @Column({ type: 'text', nullable: true, default: null })
+  recoveryCode!: string | null;
+
+  @Column({ type: 'timestamptz', nullable: true, default: null })
+  recoveryExpiresAt!: Date | null;
+
+  @Column({ type: 'boolean', nullable: true, default: null })
+  recoveryIsUsed!: boolean | null;
+
+  @Column({ type: 'timestamptz', nullable: true, default: null })
+  deleteAt!: Date | null;
+
+  @Column({ type: 'text', nullable: true, default: null })
+  refreshTokenHash!: string | null;
+}
