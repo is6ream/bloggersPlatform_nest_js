d4d98a0417514013ad63d3ce48249668126ccf82
is6ream <dnlils10@icloud.com>
2026-05-17 14:44:47 +0500

    added domain methods for blogsOrmEntity

--- src/modules/bloggers-platform/blogs/infrastructure/entity/blog-orm.entity.ts
+++ src/modules/bloggers-platform/blogs/infrastructure/entity/blog-orm.entity.ts
@@ -1,5 +1,8 @@
+import { CreateBlogDto } from './../../dto/input/createBlogDto';
 import { BaseDBEntity } from "src/core/database/base-db.entity";
 import { Column, Entity } from "typeorm";
+import { UpdateBlogDto } from "../../dto/input/updateBlogDto";
+import { randomUUID } from 'crypto';
 
 
 
@@ -8,15 +11,41 @@ export class BlogsOrmEntity extends BaseDBEntity {
     @Column({ type: 'varchar', length: 15 })
     name!: string;
 
-    @Column({ type: 'varchar', length: 255 })
+    @Column({ type: 'varchar', length: 500 })
     description!: string;
 
-    @Column({ type: 'varchar', length: 100 })
+    @Column({ type: 'varchar', name: 'websiteUrl', length: 100 })
     websiteUrl: string;
 
     @Column({ type: 'boolean', default: false })
     isMembership: boolean
 
-    @Column({ type: 'timestamptz', nullable: true, default: null})
-    deleteAt: Date
+    @Column({ type: 'timestamptz', nullable: true, default: null })
+    deleteAt: Date | null
+
+    makeDeleted(): void {
+        if (this.deleteAt !== null) {
+            throw new Error('Entity already deleted');
+        }
+        this.deleteAt = new Date()
+    }
+
+    update(dto: UpdateBlogDto): void {
+        this.name = dto.name
+        this.description = dto.description
+        this.websiteUrl = dto.websiteUrl
+    }
+
+    static create(dto: CreateBlogDto): BlogsOrmEntity {
+        const blog = new BlogsOrmEntity();
+
+        blog.id = randomUUID();
+        blog.name = dto.name;
+        blog.description = dto.description;
+        blog.websiteUrl = dto.websiteUrl;
+        blog.isMembership = false;
+        blog.deleteAt = null;
+
+        return blog;
+    }
 } 
